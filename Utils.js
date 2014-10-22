dca90d2b3f12c7145ec54fc3a988c180ac8fc630
bubuguo <bubuguo@hotmail.com>
2014-10-22 21:39:48 +0800

    Add $type

--- Utils.js
+++ Utils.js
@@ -145,7 +145,34 @@ util.filters = {
 	$exists: function(item, criteria, filterFun){
 		alert("exists");
 	},
+	$type: function(item, criteria){
+		return util.typeof(item) === criteria;
+	}
 };
+
+/*
+Type	Number	Notes
+Double	1	 
+String	2	 
+Object	3	 
+Array	4	 
+Binary data	5	 
+Undefined	6	Deprecated.
+Object id	7	 
+Boolean	8	 
+Date	9	 
+Null	10	 
+Regular Expression	11	 
+JavaScript	13	 
+Symbol	14	 
+JavaScript (with scope)	15	 
+32-bit integer	16	 
+Timestamp	17	 
+64-bit integer	18	 
+Min key	255	Query with -1.
+Max key	127
+*/
+util.typeMap = {"1": "double", "2": "string", "3": "object", "4": "array", "5": "binary data", "6": "undefined", "7": "object id", "8": "boolean", "9": "date"}
 
 util.typeof = function(obj){
 	var type = typeof obj;
